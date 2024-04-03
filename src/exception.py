5d671d9315f0622bebb128fdb491d294b18a3379
kirtika01 <rutvikkirtika@gmail.com>
2024-04-03 20:38:19 +0530

    Data Injestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -21,5 +22,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
 
