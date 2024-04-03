795100aea32e1d2798f19f0ca0c88cbfbca3179e
kirtika01 <rutvikkirtika@gmail.com>
2024-04-03 22:00:12 +0530

    transformation

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -1,5 +1,5 @@
 import os
-import sys 
+import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -7,47 +7,60 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+
 @dataclass
 class DataInjestionConfig:
-    train_data_path: str=os.path.join('artifacts', 'train.csv')
-    test_data_path: str=os.path.join('artifacts', 'test.csv')
-    raw_data_path: str=os.path.join('artifacts', 'data.csv')
-    
-    
-    
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+
 class DataInjestion:
     def __init__(self):
         self.injestion_config = DataInjestionConfig()
-        
-        
+
     def initiate_data_injestion(self):
-        logging.info('Data Injestion Started')
+        logging.info("Data Injestion Started")
         try:
-            
-            data = pd.read_csv('notebook\data\stud.csv')
-            logging.info('Data Injestion Completed')
-            
-            os.makedirs(os.path.dirname(self.injestion_config.train_data_path), exist_ok=True)
-            
+
+            data = pd.read_csv("notebook\data\stud.csv")
+            logging.info("Data Injestion Completed")
+
+            os.makedirs(
+                os.path.dirname(self.injestion_config.train_data_path), exist_ok=True
+            )
+
             data.to_csv(self.injestion_config.raw_data_path, index=False)
-            
+
             logging.info("Train test split initiated")
-            train_set,test_set = train_test_split(data, test_size=0.2, random_state=42)
-            
-            train_set.to_csv(self.injestion_config.train_data_path, index=False,header=True)
-            
-            test_set.to_csv(self.injestion_config.test_data_path, index=False,header=True)
-            
+            train_set, test_set = train_test_split(data, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.injestion_config.train_data_path, index=False, header=True
+            )
+
+            test_set.to_csv(
+                self.injestion_config.test_data_path, index=False, header=True
+            )
+
             logging.info("Train test split completed")
-            return(
+            return (
                 self.injestion_config.train_data_path,
-                self.injestion_config.test_data_path
-            
+                self.injestion_config.test_data_path,
             )
         except Exception as e:
-            
-            raise CustomException(e,sys)
-        
-if __name__=="__main__":
-    obj=DataInjestion()
-    obj.initiate_data_injestion()
+
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataInjestion()
+    train_data, test_data = obj.initiate_data_injestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
